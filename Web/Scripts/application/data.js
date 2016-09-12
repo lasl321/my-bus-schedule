19988d12399283bff4840afade905cf611a9f791
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-09-12 01:21:20 -0700

    Added query implementation

---
+++ Web/Scripts/application/data.js
@@ -0,0 +1,7 @@
+﻿(function() {
+    var module = angular.module('data', ['ngResource']);
+    var baseUrl = 'http://localhost:64154/api';
+    module.factory('data', function($resource) {
+        return $resource(baseUrl + '/arrivals/:stopId');
+    });
+}());
--- Web/Scripts/application/main.js
+++ Web/Scripts/application/main.js
@@ -1,1 +1,29 @@
-﻿
+﻿(function() {
+    'use strict';
+
+    var module = angular.module('main', ['data']);
+
+    module.controller('MainController', function($scope, $interval, data) {
+        $scope.isRunning = false;
+        $scope.messages = [];
+
+        var handle;
+        $scope.handleClick = function() {
+            $scope.isRunning = !$scope.isRunning;
+
+            if ($scope.isRunning) {
+                handle = $interval(function() {
+                    data.get({ stopId: 0 }).$promise.then(function(response) {
+                        $scope.messages.push(response);
+                    });
+                }, 1000);
+
+                return;
+            }
+
+            if (handle) {
+                $interval.cancel(handle);
+            }
+        };
+    });
+}())
