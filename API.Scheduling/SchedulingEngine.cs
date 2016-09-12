fb8b0688c61076a37eef77079fb5a330670afc85
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-09-11 22:20:51 -0700

    Refactored routes for testing

--- API.Scheduling/SchedulingEngine.cs
+++ API.Scheduling/SchedulingEngine.cs
@@ -4,20 +4,22 @@ namespace API.Scheduling
 {
     public class SchedulingEngine : ISchedulingEngine
     {
-        public SchedulingEngine() : this(TimeSpan.Zero, 10)
+        private static readonly Route[] DefaultRoutes =
+        {
+            new Route("R1", TimeSpan.FromMinutes(0)),
+            new Route("R2", TimeSpan.FromMinutes(2)),
+            new Route("R3", TimeSpan.FromMinutes(4))
+        };
+
+        public SchedulingEngine() : this(TimeSpan.Zero, 10, DefaultRoutes)
         {
         }
 
-        internal SchedulingEngine(TimeSpan startTime, int stopCount)
+        internal SchedulingEngine(TimeSpan startTime, int stopCount, Route[] routes)
         {
             StopCount = stopCount;
             StartTime = startTime;
-            Routes = new[]
-            {
-                new Route("R1", TimeSpan.FromMinutes(0)),
-                new Route("R2", TimeSpan.FromMinutes(2)),
-                new Route("R3", TimeSpan.FromMinutes(4))
-            };
+            Routes = routes;
         }
 
         public TimeSpan StartTime { get; }
