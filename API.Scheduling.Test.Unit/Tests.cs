30ea650dac84071a71aacac917fb97fe0047902c
Luis Sotomayor <luis.sotomayor@gmail.com>
2016-09-11 22:03:28 -0700

    Added sample test

---
+++ API.Scheduling.Test.Unit/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace API.Scheduling.Test.Unit
+{
+    public class Tests
+    {
+        private SchedulingEngine _sut;
+
+        public void SetUp()
+        {
+            _sut = new SchedulingEngine();
+        }
+
+        [Test]
+        public void Should_2016_09_11_10_02_29()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
